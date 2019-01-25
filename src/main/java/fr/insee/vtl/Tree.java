6f0ef4f77613a91019aae8a8a944d51c381cb89b
Franck Cotton <franck.cotton@insee.fr>
2019-01-25 10:55:08 +0100

    First version

---
+++ src/main/java/fr/insee/vtl/Tree.java
@@ -0,0 +1,28 @@
+package fr.insee.vtl;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("tree")
+public class Tree {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt(@QueryParam("expression") String expression) {
+        return expression;
+    }
+}
