e609654e6dfb80973e88d3325165d498c039e204
Franck Cotton <franck.cotton@insee.fr>
2019-01-31 14:49:13 +0100

    Added basic Jersey logging

--- src/main/java/fr/insee/vtl/ParserResourceConfig.java
+++ src/main/java/fr/insee/vtl/ParserResourceConfig.java
@@ -1,12 +1,17 @@
 package fr.insee.vtl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ws.rs.ApplicationPath;
 
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("parser")
 public class ParserResourceConfig extends ResourceConfig {
     public ParserResourceConfig() {
         packages("fr.insee.vtl");
+        register(new LoggingFeature(Logger.getLogger(LoggingFeature.DEFAULT_LOGGER_NAME), Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, 10000));
     }
 }
