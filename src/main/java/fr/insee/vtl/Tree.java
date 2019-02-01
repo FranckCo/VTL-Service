2863ff3309d11e79c72281e45ed016be61818b8d
Benoît Werquin <benoit.werquin@insee.fr>
2019-02-01 14:40:46 +0100

    Add decode url before parsing

--- src/main/java/fr/insee/vtl/Tree.java
+++ src/main/java/fr/insee/vtl/Tree.java
@@ -1,7 +1,5 @@
 package fr.insee.vtl;
 
-import java.io.IOException;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -36,28 +34,22 @@ public class Tree {
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getIt(@QueryParam("expression") String expression) throws Exception {
-
 		String tree = null;
-
 		try {
-
+			//Decode URL 
+			String decodedExpression = java.net.URLDecoder.decode(expression, "UTF-8");
 			// Create a VTL lexer instance
-			VtlLexer lexer = new VtlLexer(CharStreams.fromString(expression));
+			VtlLexer lexer = new VtlLexer(CharStreams.fromString(decodedExpression));
 			// Create a VTL parser instance
 			VtlParser parser = new VtlParser(new CommonTokenStream(lexer));
 			// Create a context for rule "start" (root rule for VTL)
 			StartContext context = parser.start();
-
-			// Return the parse tree as a string
+			// Parse tree as a string
 			tree = context.toStringTree(parser);
-
-			
 		} catch (RecognitionException e) {
-
 			throw new VTLServiceException(400, e.getOffendingState(), e.getExpectedTokens().getMinElement() + " "
 					+ e.getExpectedTokens().getMaxElement() + "- " + e.getInputStream().size(), null);
 		}
-
 		// Return the parse tree as a string
 		return tree;
 	}
