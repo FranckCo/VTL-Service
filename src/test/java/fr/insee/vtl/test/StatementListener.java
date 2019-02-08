856ff0b206293df2277bd0e81ac4174c26012abf
Franck Cotton <franck.cotton@insee.fr>
2019-02-08 10:27:42 +0100

    Refined listener test

--- src/test/java/fr/insee/vtl/test/StatementListener.java
+++ src/test/java/fr/insee/vtl/test/StatementListener.java
@@ -2,19 +2,32 @@ package fr.insee.vtl.test;
 
 import fr.insee.vtl.VtlBaseListener;
 import fr.insee.vtl.VtlParser;
+import fr.insee.vtl.VtlParser.ExprContext;
 
 public class StatementListener extends VtlBaseListener {
 
     @Override
     public void enterStatement(VtlParser.StatementContext context) {
 
-    	System.out.println("Entering statement");
+    	System.out.println("Entering 'statement' rule, text is " + context.getText());
     }
 
     @Override
     public void exitStatement(VtlParser.StatementContext context) {
 
-    	System.out.println("Exiting statement");
+    	System.out.println("Exiting 'statement' rule");
+    }
+
+    @Override
+    public void enterExpr(ExprContext context) {
+
+    	System.out.println("Entering 'expr' rule, text is " + context.getText());
+    }
+
+    @Override
+    public void exitExpr(ExprContext context) {
+
+    	System.out.println("Exiting 'expr' rule");
     }
 
 }
