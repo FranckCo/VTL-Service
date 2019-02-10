6e3af7eda8564271086a6e420f9d7b6fd34a3801
Franck Cotton <franck.cotton@insee.fr>
2019-02-10 17:40:49 +0100

    More methods in statement test

--- src/test/java/fr/insee/vtl/test/StatementListener.java
+++ src/test/java/fr/insee/vtl/test/StatementListener.java
@@ -1,10 +1,16 @@
 package fr.insee.vtl.test;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import fr.insee.vtl.VtlBaseListener;
+import fr.insee.vtl.VtlLexer;
 import fr.insee.vtl.VtlParser;
+import fr.insee.vtl.VtlParser.CalcClauseContext;
+import fr.insee.vtl.VtlParser.CalcClauseItemContext;
 import fr.insee.vtl.VtlParser.ExprContext;
 
 public class StatementListener extends VtlBaseListener {
@@ -15,7 +21,7 @@ public class StatementListener extends VtlBaseListener {
     public void enterStatement(VtlParser.StatementContext context) {
 
     	logger.info("Entering 'statement' rule, text is " + context.getText());
-    	logger.info(context.getSourceInterval().toString());
+    	this.logContext(context);
     }
 
     @Override
@@ -28,6 +34,7 @@ public class StatementListener extends VtlBaseListener {
     public void enterExpr(ExprContext context) {
 
     	logger.info("Entering 'expr' rule, text is " + context.getText());
+    	this.logContext(context);
     }
 
     @Override
@@ -36,4 +43,39 @@ public class StatementListener extends VtlBaseListener {
     	logger.info("Exiting 'expr' rule");
     }
 
+    @Override
+    public void enterCalcClause(CalcClauseContext context) {
+
+    	logger.info("Entering 'calcClause' rule, text is " + context.getText());
+    	this.logContext(context);
+    }
+
+    @Override
+    public void exitCalcClause(CalcClauseContext context) {
+
+    	logger.info("Exiting 'calcClause' rule");
+    }
+
+    @Override
+    public void enterCalcClauseItem(CalcClauseItemContext context) {
+
+    	logger.info("Entering 'calcClauseItem' rule, text is " + context.getText());
+    	this.logContext(context);
+    }
+
+    @Override
+    public void exitCalcClauseItem(CalcClauseItemContext context) {
+
+    	logger.info("Exiting 'calcClauseItem' rule");
+    }
+
+    private void logContext(ParserRuleContext context) {
+
+    	logger.info("The statement correspond to tokens " + context.getSourceInterval().toString());
+    	for (int childIndex = 0; childIndex < context.getChildCount(); childIndex++) {
+    		ParseTree childTree = context.getChild(childIndex);
+    		logger.info("Child number " + childIndex + " has type " + childTree.getClass() + " and text " + childTree.getText());
+    	}
+    	for (TerminalNode terminal : context.getTokens(VtlLexer.ASSIGN)) logger.info("Terminal assignments: " + terminal.getText());
+    }
 }
