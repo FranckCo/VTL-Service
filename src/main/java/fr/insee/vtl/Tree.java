323b4d1968bb167a96263e524562ecbc5b1defca
Franck Cotton <franck.cotton@insee.fr>
2019-05-14 12:51:59 +0200

    First try with PostScript

--- src/main/java/fr/insee/vtl/Tree.java
+++ src/main/java/fr/insee/vtl/Tree.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
@@ -17,7 +18,7 @@ import fr.insee.vtl.VtlParser.StartContext;
 import fr.insee.vtl.exception.VTLServiceException;
 
 /**
- * Root resource (exposed at "vtl-parser" path)
+ * Root resource (exposed at "parser" path)
  */
 @Path("tree")
 public class Tree {
@@ -30,35 +31,50 @@ public class Tree {
 	 * @throws Exception
 	 */
 	@GET
-	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getText(@QueryParam("expression") String expression) throws Exception {
+
 		String tree = null;
 		try {
-			tree = getTree(URLDecoder.decode(expression, "UTF-8"));
+			tree = getTree(URLDecoder.decode(expression, "UTF-8")).toStringTree();
 		} catch (RecognitionException e) {
 			throw new VTLServiceException(400, e.getOffendingState(), e.getExpectedTokens().getMinElement() + " "
-					+ e.getExpectedTokens().getMaxElement() + "- " + e.getInputStream().size(), null);
+					+ e.getExpectedTokens().getMaxElement() + " - " + e.getInputStream().size(), null);
 		}
 		// Return the parse tree as a string
 		return tree;
 	}
 
-	@Consumes(MediaType.TEXT_PLAIN)
+	/**
+	 * Method handling HTTP GET requests for "application/postscript" media type.
+	 * 
+	 * @param expression A VTL 2.0 expression.
+	 * @return The parse tree corresponding to the expression, returned as a text/plain response.
+	 * @throws Exception
+	 */
+	@GET
 	@Produces("application/postscript")
-	public String getPS(@QueryParam("expression") String expression) {
-		return null;
-		
+	public String getPS(@QueryParam("expression") String expression) throws Exception {
+
+		String psTree = null;
+		try {
+			psTree = Trees.getPS(getTree(URLDecoder.decode(expression, "UTF-8")), null);
+		} catch (RecognitionException e) {
+			throw new VTLServiceException(400, e.getOffendingState(), e.getExpectedTokens().getMinElement() + " "
+					+ e.getExpectedTokens().getMaxElement() + "- " + e.getInputStream().size(), null);
+		}
+		// Return the parse tree as a PostScript string
+		return psTree;
 	}
 
 	/**
-	 * Parses a VTL expression and returns the corresponding parse tree as a string.
+	 * Parses a VTL expression and returns the corresponding parse tree.
 	 * 
 	 * @param expression The VTL 2.0 expression to parse.
-	 * @return The parse tree as a string.
-	 * @throws RecognitionException In case of problem while parsing the exception.
+	 * @return The parse tree as a <code>StartContext</code> instance.
+	 * @throws RecognitionException In case of problem while parsing the expression.
 	 */
-	private String getTree(String expression) throws RecognitionException {
+	private StartContext getTree(String expression) throws RecognitionException {
 
 		// Create a VTL lexer instance
 		VtlLexer lexer = new VtlLexer(CharStreams.fromString(expression));
@@ -66,7 +82,7 @@ public class Tree {
 		VtlParser parser = new VtlParser(new CommonTokenStream(lexer));
 		// Create a context for rule "start" (root rule for VTL)
 		StartContext context = parser.start();
-		// Return parse tree as a string
-		return context.toStringTree(parser);
+		// Return parse tree
+		return context;
 	}
 }
--- src/test/java/fr/insee/vtl/test/TestParser.java
+++ src/test/java/fr/insee/vtl/test/TestParser.java
@@ -1,5 +1,9 @@
 package fr.insee.vtl.test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.apache.logging.log4j.LogManager;
@@ -31,4 +35,23 @@ public class TestParser {
 
 		logger.info(tree);
 	}
+
+	@Test
+	public void testPostScript() {
+
+		final String vtlExpression = "DS_r := DS_1 [ calc Me_2 := upper ( Me_1 ) ]";
+
+		// Create VTL lexer and parser instances
+		lexer = new VtlLexer(CharStreams.fromString(vtlExpression));
+		parser = new VtlParser(new CommonTokenStream(lexer));
+		// Create a context for rule "start" (root rule for VTL)
+		StartContext context = parser.start();
+		List<String> ruleNames = Arrays.asList(parser.getRuleNames());
+		System.out.println(ruleNames);
+		// Return tree as PostScript
+		String psTree = Trees.getPS(context, ruleNames);
+
+		System.out.println(psTree);
+	}
+	
 }
